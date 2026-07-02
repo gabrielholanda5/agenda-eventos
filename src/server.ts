7f1262021b61ea5a8682b9cbb5cbcb892d6f3787
Gabriel Holanda <gabrielholanda.work@hotmail.com>
2026-07-02 10:22:20 -0300

    correcao

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use("/eventos", eventoRoutes)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando lindamente em http://localhost:${PORT}`)
 })
